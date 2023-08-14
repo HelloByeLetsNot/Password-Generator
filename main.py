4acf98980ae510c4fb9892806b3619c7a3237b91
Stephen <70440468+AstralSkies@users.noreply.github.com>
2023-08-14 20:48:24 +0100

    Update main.py
    Include options for uppercase, digits, and special characters in GUI.
    
    Add dark mode toggle feature.
    
    Introduce password strength analysis.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 import random
 import string
+from tkinter import messagebox
 
 def generate_password(length=12, use_uppercase=True, use_digits=True, use_special_chars=True):
+    """Function to generate a password."""
     characters = string.ascii_lowercase
     if use_uppercase:
         characters += string.ascii_uppercase
@@ -14,7 +16,17 @@ def generate_password(length=12, use_uppercase=True, use_digits=True, use_specia
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
+def password_strength(password):
+    """Function to determine password strength."""
+    strength = "Weak"
+    if len(password) >= 8:
+        strength = "Medium"
+        if any(c.isdigit() for c in password) and any(c.isupper() for c in password) and any(c in string.punctuation for c in password):
+            strength = "Strong"
+    return strength
+
 def generate_button_click():
+    """Function to handle the generate button click."""
     length = int(length_var.get())
     use_uppercase = uppercase_var.get()
     use_digits = digits_var.get()
@@ -22,12 +34,44 @@ def generate_button_click():
 
     password = generate_password(length, use_uppercase, use_digits, use_special_chars)
     password_var.set(password)
+    strength_var.set(f"Strength: {password_strength(password)}")
+
+def copy_password():
+    """Function to copy password to clipboard."""
+    root.clipboard_clear()
+    root.clipboard_append(password_var.get())
+    root.update()
+    messagebox.showinfo("Copied", "Password copied to clipboard!")
+
+def toggle_dark_mode():
+    """Function to toggle dark mode."""
+    if dark_mode_var.get():
+        set_dark_mode()
+    else:
+        set_light_mode()
+
+def set_dark_mode():
+    """Function to apply dark mode."""
+    root.configure(bg="black")
+    password_label.configure(bg="black", fg="white")
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Label, tk.Checkbutton, tk.Button)):
+            widget.configure(bg="black", fg="white")
+
+def set_light_mode():
+    """Function to apply light mode."""
+    root.configure(bg="white")
+    password_label.configure(bg="white", fg="black")
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Label, tk.Checkbutton, tk.Button)):
+            widget.configure(bg="white", fg="black")
 
 # Create the main window
 root = tk.Tk()
 root.title("HelloBye - Password Generator")
 
-# Create and place widgets
+# <!-- UI Elements -->
+
 tk.Label(root, text="Password Length:").pack()
 length_var = tk.StringVar(value="12")
 length_entry = tk.Entry(root, textvariable=length_var)
@@ -45,10 +89,22 @@ tk.Checkbutton(root, text="Include Special Characters", variable=special_chars_v
 generate_button = tk.Button(root, text="Generate Password", command=generate_button_click)
 generate_button.pack()
 
+copy_button = tk.Button(root, text="Copy Password", command=copy_password)
+copy_button.pack()
+
 password_var = tk.StringVar()
 password_var.set("Your generated password will appear here.")
 password_label = tk.Label(root, textvariable=password_var, wraplength=300)
 password_label.pack()
+
+strength_var = tk.StringVar()
+strength_label = tk.Label(root, textvariable=strength_var)
+strength_label.pack()
+
+dark_mode_var = tk.BooleanVar(value=False)
+tk.Checkbutton(root, text="Toggle Dark Mode", variable=dark_mode_var, command=toggle_dark_mode).pack()
+
+# <!-- End of UI Elements -->
 
 # Start the main event loop
 root.mainloop()
